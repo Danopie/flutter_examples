630076cf74e52817fce255fcbef54b56c0f81a12
DanLe <danle.sdev@gmail.com>
2020-07-20 17:41:35 +0700

    Update slivers

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,31 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_portal/flutter_portal.dart';
+import 'package:for_research/hooks.dart';
+import 'package:for_research/journey_demo.dart';
 import 'package:for_research/neumorphic_animated_player/colors.dart';
-
-import 'anchored_circle_route.dart';
+import 'package:for_research/same_key.dart';
+import 'package:for_research/slivers.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(DevicePreview(
+    builder: (_) => MyApp(),
+    enabled: true,
+  ));
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: DevicePreview.appBuilder,
+
       theme: Theme.of(context).copyWith(
         backgroundColor: kPrimary,
         scaffoldBackgroundColor: kPrimary,
         dialogBackgroundColor: kPrimary,
       ),
-      home: AnchoredRouteDemo(),
+      home: AppBarSnapDemo(),
     );
   }
 }
--- lib/slivers.dart
+++ lib/slivers.dart
@@ -1,5 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_state_notifier/flutter_state_notifier.dart';
+import 'package:state_notifier/state_notifier.dart';
+import 'package:provider/provider.dart';
 
 class AppBarSnapDemo extends StatefulWidget {
   @override
@@ -9,74 +15,265 @@ class AppBarSnapDemo extends StatefulWidget {
 class _AppBarSnapDemoState extends State<AppBarSnapDemo>
     with SingleTickerProviderStateMixin {
   ScrollController _controller = ScrollController();
-  List<int> items = List<int>();
+  final controller = ItemListController();
 
   @override
   void initState() {
-    items = _getInitialList();
-    _controller.addListener(() {
-      if (_controller.offset > _controller.position.maxScrollExtent - 100) {
-        loadMore();
-      }
-    });
+    controller.init();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(
-        controller: _controller,
-        slivers: <Widget>[
-          SliverPersistentHeader(
-            pinned: true,
-            delegate: HeaderDelegate(this, _controller),
-          ),
-          CupertinoSliverRefreshControl(
-            onRefresh: () async {
-              await Future.delayed(Duration(seconds: 1));
-              setState(() {
-                items = _getInitialList();
-              });
-            },
-          ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) {
-                return ListTile(
-                  title: Text("Item ${items[index]}"),
-                );
-              },
-              childCount: items.length,
+    return StateNotifierProvider<ItemListController, ItemListState>(
+      create: (_) => controller,
+      child: Builder(
+        builder: (context) {
+          final state = context.watch<ItemListState>();
+          final items = state.items;
+          return Scaffold(
+            body: Column(
+              children: [
+                Expanded(
+                  child: AwesomeListView(
+                    controller: _controller,
+                    onRefresh: context.watch<ItemListController>().onRefresh,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: EdgeInsets.all(12),
+                        height: 60,
+                        color: Colors.green,
+                        child: ListTile(
+                          title: Text("Item $index"),
+                        ),
+                      );
+                    },
+                    itemCount: items?.length,
+                    emptyBuilder: (_) => Center(
+                      child: Text(state.id == ItemListState.Loading
+                          ? "Loading"
+                          : "Empty"),
+                    ),
+                    onPagingLoad: context.watch<ItemListController>().loadMore,
+                    headerBuilder: (context, shrinkOffset, overlapsContent) =>
+                        Placeholder(),
+                    headerMaxExtent: 148,
+                    headerMinExtent: 80,
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
+}
 
-  List<int> _getInitialList() {
-    return List<int>.generate(10, (index) => index);
-  }
+class ItemListState {
+  static const String Loading = "Loading";
+  static const String DoneLoading = "DoneLoading";
 
-  void loadMore() {
-    setState(() {
-      items.addAll(_getInitialList());
-    });
+  final String id;
+  final List<int> items;
+
+  const ItemListState({
+    @required this.id,
+    this.items,
+  });
+
+  ItemListState copyWith({
+    String id,
+    List<int> items,
+  }) {
+    return new ItemListState(
+      id: id ?? this.id,
+      items: items ?? this.items,
+    );
   }
 }
 
-class HeaderDelegate extends SliverPersistentHeaderDelegate {
-  final TickerProvider vsync;
-  final ScrollController scrollController;
+class ItemListController extends StateNotifier<ItemListState> {
+  ItemListController() : super(ItemListState(id: ItemListState.Loading));
 
-  HeaderDelegate(this.vsync, this.scrollController);
+  Future<void> init() async {
+    await onRefresh();
+  }
+
+  List<int> _getInitialList() {
+    return List<int>.generate(2, (index) => index);
+  }
+
+  Future<void> loadMore() async {
+    print('ItemListController.loadMore');
+    if (state.items != null && state.items.isNotEmpty) {
+      await Future.delayed(Duration(milliseconds: 200));
+      final newList = List.of(state.items)..addAll(_getInitialList());
+
+      state = state.copyWith(items: newList);
+    }
+  }
+
+  Future<void> onRefresh() async {
+    print('ItemListController.onRefresh');
+    await Future.delayed(Duration(seconds: 1));
+    state =
+        state.copyWith(id: ItemListState.DoneLoading, items: _getInitialList());
+  }
+}
+
+class AwesomeListView<T> extends StatefulWidget {
+  final ScrollController controller;
+  final Function onRefresh;
+  final IndexedWidgetBuilder itemBuilder;
+  final int itemCount;
+  final WidgetBuilder emptyBuilder;
+  final bool pinned;
+  final bool floating;
+  final Function onPagingLoad;
+  final ScrollHeaderBuilder headerBuilder;
+  final double headerMaxExtent;
+  final double headerMinExtent;
+  final double pagingTriggerOffset;
+
+  const AwesomeListView({
+    Key key,
+    this.controller,
+    this.onRefresh,
+    this.itemBuilder,
+    this.itemCount,
+    this.emptyBuilder,
+    this.pinned = true,
+    this.floating = false,
+    this.onPagingLoad,
+    this.headerBuilder,
+    this.headerMaxExtent,
+    this.headerMinExtent,
+    this.pagingTriggerOffset = 100,
+  }) : super(key: key);
+
+  @override
+  _AwesomeListViewState createState() => _AwesomeListViewState();
+}
+
+class _AwesomeListViewState extends State<AwesomeListView> {
+  final Throttling thr = new Throttling(duration: Duration(milliseconds: 500));
+  ScrollController _controller;
+
+  @override
+  void initState() {
+    _controller = widget.controller ?? ScrollController();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return NotificationListener<ScrollNotification>(
+      onNotification: (scrollNotification) {
+
+        final userReachingMaxExtent = scrollNotification.metrics.pixels >
+            scrollNotification.metrics.maxScrollExtent -
+                widget.pagingTriggerOffset;
+
+        final userIsScrollingForward =
+            _controller.position.userScrollDirection == ScrollDirection.reverse;
+
+        if (userReachingMaxExtent && userIsScrollingForward) {
+          if (widget.onPagingLoad != null) {
+            thr.throttle(() {
+              widget.onPagingLoad();
+            });
+          }
+          return true;
+        }
+        return false;
+      },
+      child: RefreshIndicatorIfAndroid(
+        onRefresh: widget.onRefresh,
+        child: CustomScrollView(
+          controller: _controller,
+          physics: AlwaysScrollableScrollPhysics(
+              parent: Theme.of(context).platform == TargetPlatform.iOS
+                  ? CustomBouncingScrollPhysics()
+                  : CustomClampingScrollPhysics()),
+          slivers: <Widget>[
+            if (widget.headerBuilder != null)
+              SliverPersistentHeader(
+                pinned: widget.pinned,
+                floating: widget.floating,
+                delegate: _ScrollHeaderDelegate(
+                  _controller,
+                  widget.headerBuilder,
+                  widget.headerMaxExtent,
+                  widget.headerMinExtent,
+                ),
+              ),
+            if (Theme.of(context).platform == TargetPlatform.iOS &&
+                widget.onRefresh != null)
+              CupertinoSliverRefreshControl(
+                onRefresh: widget.onRefresh,
+              ),
+            if (widget.itemCount != null && widget.itemCount > 0)
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  widget.itemBuilder,
+                  childCount: widget.itemCount,
+                ),
+              )
+            else
+              SliverFillRemaining(
+                child: widget.emptyBuilder(context),
+                hasScrollBody: false,
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RefreshIndicatorIfAndroid extends StatelessWidget {
+  final Widget child;
+  final Function onRefresh;
+
+  const RefreshIndicatorIfAndroid({Key key, this.child, this.onRefresh})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (Theme.of(context).platform == TargetPlatform.android &&
+        onRefresh != null) {
+      return RefreshIndicator(
+        child: child,
+        onRefresh: onRefresh,
+      );
+    } else {
+      return child;
+    }
+  }
+}
+
+typedef ScrollHeaderBuilder = Widget Function(
+    BuildContext context, double shrinkOffset, bool overlapsContent);
+
+class _ScrollHeaderDelegate extends SliverPersistentHeaderDelegate {
+  final ScrollController scrollController;
+  final ScrollHeaderBuilder headerBuilder;
+  final double headerMaxExtent;
+  final double headerMinExtent;
+
+  _ScrollHeaderDelegate(
+    this.scrollController,
+    this.headerBuilder,
+    this.headerMaxExtent,
+    this.headerMinExtent,
+  );
 
   @override
   Widget build(
       BuildContext context, double shrinkOffset, bool overlapsContent) {
     return ScrollViewSnapper(
-      child: Placeholder(),
+      child: headerBuilder(context, shrinkOffset, overlapsContent),
       maxHeight: 148,
       minHeight: 80,
       controller: scrollController,
@@ -84,10 +281,10 @@ class HeaderDelegate extends SliverPersistentHeaderDelegate {
   }
 
   @override
-  double get maxExtent => 148;
+  double get maxExtent => headerMaxExtent;
 
   @override
-  double get minExtent => 80;
+  double get minExtent => headerMinExtent;
 
   @override
   bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
@@ -96,9 +293,13 @@ class HeaderDelegate extends SliverPersistentHeaderDelegate {
 }
 
 class ScrollViewSnapper extends StatefulWidget {
-  const ScrollViewSnapper(
-      {Key key, this.child, this.maxHeight, this.minHeight, this.controller})
-      : super(key: key);
+  const ScrollViewSnapper({
+    Key key,
+    this.child,
+    this.maxHeight,
+    this.minHeight,
+    this.controller,
+  }) : super(key: key);
 
   final Widget child;
   final double maxHeight;
@@ -109,7 +310,8 @@ class ScrollViewSnapper extends StatefulWidget {
   _ScrollViewSnapperState createState() => _ScrollViewSnapperState();
 }
 
-class _ScrollViewSnapperState extends State<ScrollViewSnapper> {
+class _ScrollViewSnapperState extends State<ScrollViewSnapper>
+    with TickerProviderStateMixin {
   ScrollPosition _position;
 
   @override
@@ -120,36 +322,154 @@ class _ScrollViewSnapperState extends State<ScrollViewSnapper> {
     _position = Scrollable.of(context)?.position;
     if (_position != null)
       _position.isScrollingNotifier.addListener(_isScrollingListener);
+
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 200))
+          ..addListener(() {
+            widget.controller.jumpTo(_animation.value);
+          });
   }
 
   @override
   void dispose() {
     if (_position != null)
       _position.isScrollingNotifier.removeListener(_isScrollingListener);
+
     super.dispose();
   }
 
-  void _isScrollingListener() {
+  Animation<double> _animation;
+  AnimationController _animationController;
+
+  Future<void> _isScrollingListener() async {
     if (_position == null) return;
 
-    if (!_position.isScrollingNotifier.value) {
+    if (!_position.isScrollingNotifier.value &&
+        !(_position.activity is HoldScrollActivity)) {
       final heightDelta = widget.maxHeight - widget.minHeight;
-      print(
-          'Stop scrolling: offset at ${_position.pixels} comparing to ${widget.maxHeight - widget.minHeight}');
-      if (_position.pixels > 0 && _position.pixels < heightDelta) {
-        print('Start snap');
-        double snapDistance;
+
+      if (_position.pixels >= 0 && _position.pixels < heightDelta) {
+        double snapOffset;
         if (_position.pixels / heightDelta > 0.5) {
-          snapDistance = heightDelta;
+          snapOffset = heightDelta;
         } else {
-          snapDistance = 0;
+          snapOffset = 0;
         }
-        widget.controller.animateTo(snapDistance,
-            duration: Duration(milliseconds: 200), curve: Curves.easeIn);
+
+        if (_position.pixels != snapOffset) {
+          _animation = _animationController.drive(
+              Tween<double>(begin: _position.pixels, end: snapOffset)
+                  .chain(CurveTween(curve: Curves.easeIn)));
+
+          _animationController.forward(from: 0.0);
+        }
       }
+    } else {
+      _animationController?.stop();
     }
   }
 
   @override
   Widget build(BuildContext context) => widget.child;
+}
+
+class CustomClampingScrollPhysics extends ClampingScrollPhysics {
+  CustomClampingScrollPhysics({
+    ScrollPhysics parent,
+  }) : super(parent: parent);
+
+  @override
+  ClampingScrollPhysics applyTo(ScrollPhysics ancestor) {
+    return CustomClampingScrollPhysics(
+      parent: buildParent(ancestor),
+    );
+  }
+
+  @override
+  double applyBoundaryConditions(ScrollMetrics position, double value) {
+    if (value < position.pixels &&
+        position.pixels <= position.minScrollExtent) {
+      return value - position.pixels;
+    } // underscroll
+
+    if (position.maxScrollExtent <= position.pixels &&
+        position.pixels < value) {
+      if (position.maxScrollExtent == 0 && value > 10) {
+        return value - position.pixels;
+      }
+    } // overscroll
+
+    if (value < position.minScrollExtent &&
+        position.minScrollExtent < position.pixels) {
+      return value - position.minScrollExtent;
+    } // hit top edge
+
+    return 0.0;
+  }
+}
+
+class CustomBouncingScrollPhysics extends BouncingScrollPhysics {
+  CustomBouncingScrollPhysics({
+    ScrollPhysics parent,
+  }) : super(parent: parent);
+
+  @override
+  CustomBouncingScrollPhysics applyTo(ScrollPhysics ancestor) {
+    return CustomBouncingScrollPhysics(
+      parent: buildParent(ancestor),
+    );
+  }
+
+  @override
+  double applyBoundaryConditions(ScrollMetrics position, double value) {
+    if (position.maxScrollExtent <= position.pixels &&
+        position.pixels < value) {
+      if (position.maxScrollExtent == 0 && value > 10) {
+        return value - position.pixels;
+      }
+    }
+
+    return 0.0;
+  }
+}
+
+class Throttling {
+  Duration _duration;
+  Duration get duration => this._duration;
+  set duration(Duration value) {
+    assert(duration is Duration && !duration.isNegative);
+    this._duration = value;
+  }
+
+  bool _isReady = true;
+  bool get isReady => isReady;
+  Future<void> get _waiter => Future.delayed(this._duration);
+  // ignore: close_sinks
+  final StreamController<bool> _stateSC =
+      new StreamController<bool>.broadcast();
+
+  Throttling({Duration duration = const Duration(seconds: 1)})
+      : assert(duration is Duration && !duration.isNegative),
+        this._duration = duration ?? Duration(seconds: 1) {
+    this._stateSC.sink.add(true);
+  }
+
+  dynamic throttle(Function func) {
+    if (!this._isReady) return null;
+    this._stateSC.sink.add(false);
+    this._isReady = false;
+    _waiter
+      ..then((_) {
+        this._isReady = true;
+        this._stateSC.sink.add(true);
+      });
+    return Function.apply(func, []);
+  }
+
+  StreamSubscription<bool> listen(Function(bool) onData) =>
+      this._stateSC.stream.listen(onData);
+
+  dispose() {
+    this._stateSC.close();
+  }
 }
